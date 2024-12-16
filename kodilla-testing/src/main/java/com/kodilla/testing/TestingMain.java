7ff628b8a2c74871808c334fe3a894888d06464b
Peter <piowiedlocha@gmail.com>
2024-12-16 11:30:00 +0100

    calculator test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,36 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+
+        int addResult = calculator.add(5, 3);
+        if (addResult == 8) {
+            System.out.println("Test dodawania: OK");
+        } else {
+            System.out.println("Test dodawania: BŁĄD");
+        }
+
+        int subtractResult = calculator.subtract(10, 4);
+        if (subtractResult == 6) {
+            System.out.println("Test odejmowania: OK");
+        } else {
+            System.out.println("Test odejmowania: BŁĄD");
+        }
+
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    // Metoda dodawania
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // Metoda odejmowania
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}

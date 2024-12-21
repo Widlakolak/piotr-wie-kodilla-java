8aadcfd282b0bca23f8226ab4d23fb97b0159cc1
Peter <piowiedlocha@gmail.com>
2024-12-22 00:27:47 +0100

    7 - stream Lambda Poem decorator

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,36 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+public class StreamMain {
+
+    public static void main(String[] args) {
+
+//        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+//
+//        System.out.println("Calculating expressions with lambdas");
+//        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+//        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+//        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+//        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+//
+//        System.out.println("Calculating expressions with method references");
+//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        System.out.println("Poem beautifier test with lambda expression");
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+
+        poemBeautifier.beautify("stream text for Lambda practice", poem -> "ABS" + poem + "ABC");
+        poemBeautifier.beautify("stream text for Lambda practice", String::toUpperCase);
+        poemBeautifier.beautify("stream text for Lambda practice", String::toLowerCase);
+        poemBeautifier.beautify("stream text for Lambda practice", poem -> "7.1 - " + poem + ".");
+
+
+    }
+}

4f471fe7e842bd8ef0405c0f0a942c31d1f8d7a0
Peter <piowiedlocha@gmail.com>
2024-12-22 01:14:14 +0100

    7.2 - Stream

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -22,15 +23,17 @@ public class StreamMain {
 //        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
 //        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
 
-        System.out.println("Poem beautifier test with lambda expression");
+//        System.out.println("Poem beautifier test with lambda expression");
+//
+//        PoemBeautifier poemBeautifier = new PoemBeautifier();
+//
+//        poemBeautifier.beautify("stream text for Lambda practice", poem -> "ABS" + poem + "ABC");
+//        poemBeautifier.beautify("stream text for Lambda practice", String::toUpperCase);
+//        poemBeautifier.beautify("stream text for Lambda practice", String::toLowerCase);
+//        poemBeautifier.beautify("stream text for Lambda practice", poem -> "7.1 - " + poem + ".");
 
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
 
-        poemBeautifier.beautify("stream text for Lambda practice", poem -> "ABS" + poem + "ABC");
-        poemBeautifier.beautify("stream text for Lambda practice", String::toUpperCase);
-        poemBeautifier.beautify("stream text for Lambda practice", String::toLowerCase);
-        poemBeautifier.beautify("stream text for Lambda practice", poem -> "7.1 - " + poem + ".");
-
-
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }

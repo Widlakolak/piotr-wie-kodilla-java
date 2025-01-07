c868a87c37b142e0c67d166b5df69cd1f14f37f6
Peter <piowiedlocha@gmail.com>
2025-01-08 00:33:02 +0100

    8.3 exception test SecondChallenge fix

--- kodilla-exception/src/test/java/com/kodilla/exception/io/SecondChallengeTestSuite.java
+++ kodilla-exception/src/test/java/com/kodilla/exception/io/SecondChallengeTestSuite.java
@@ -1,7 +1,6 @@
 package com.kodilla.exception.io;
 
-import com.kodilla.exception.test.SecondChallenge;
-import com.kodilla.exception.test.SecondChallengeException;
+import com.kodilla.exception.test.ExceptionHandling;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,14 +10,14 @@ public class SecondChallengeTestSuite {
     @Test
     void testSecondChallengeWithNumbers() {
         //Given
-        SecondChallenge secondChallenge = new SecondChallenge();
+        ExceptionHandling ex = new ExceptionHandling();
 
         //When & Them
         assertAll(
-                () -> assertThrows(Exception.class, () -> secondChallenge.probablyIWillThrowException(2.1, 1.5)),
-                () -> assertThrows(Exception.class, () -> secondChallenge.probablyIWillThrowException(2.1, 4)),
-                () -> assertThrows(Exception.class, () -> secondChallenge.probablyIWillThrowException(1.5, 1.5)),
-                () -> assertDoesNotThrow(() -> secondChallenge.probablyIWillThrowException(1.5, 4))
+                () -> assertThrows(Exception.class, () -> ex.handleSecondChallenge(2.1, 1.5)),
+                () -> assertThrows(Exception.class, () -> ex.handleSecondChallenge(2.1, 4)),
+                () -> assertThrows(Exception.class, () -> ex.handleSecondChallenge(1.5, 1.5)),
+                () -> assertDoesNotThrow(() -> ex.handleSecondChallenge(1.5, 4))
                 );
     }
 }
